e1f83f7eed1226e90492ec232dbbbc258c042b13
tjabitta <tunji.jabitta@gmail.com>
2019-11-14 12:16:00 +0000

    Added a new unit test cause the initial one was in another folder.

---
+++ PriceEngineTest/PriceEngineTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ConsoleApp1.Domain.Interfaces;
+
+namespace PriceEngineTest
+{
+    [TestClass]
+    public class PriceEngineTest
+    {
+
+        [TestInitialize()]
+        public void Initialize()
+        {
+        }
+        [TestMethod]
+        public void TestGetPrice()
+        {
+
+        }
+    }
+}
---
+++ PriceEngineTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PriceEngineTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PriceEngineTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("abf657a3-c4c2-4e7f-a0b1-6fbb528bb599")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
