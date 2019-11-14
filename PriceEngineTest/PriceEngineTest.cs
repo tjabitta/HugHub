9df14a96ddff5b5c69e4aca2723fb9885d3a4d2e
tjabitta <tunji.jabitta@gmail.com>
2019-11-14 12:21:53 +0000

    Advancing the unit test!

--- PriceEngineTest/PriceEngineTest.cs
+++ PriceEngineTest/PriceEngineTest.cs
@@ -2,6 +2,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using ConsoleApp1.Domain.Interfaces;
+using ConsoleApp1.Domain.Models;
+using ConsoleApp1.Domain.Services;
 
 namespace PriceEngineTest
 {
@@ -12,11 +14,31 @@ namespace PriceEngineTest
         [TestInitialize()]
         public void Initialize()
         {
+
         }
         [TestMethod]
         public void TestGetPrice()
         {
+            decimal actualPrice = Decimal.Parse("92.67");
+            var request = new PriceRequest()
+            {
+                RiskData = new RiskData()
+                {
+                    DOB = DateTime.Parse("1980-01-01"),
+                    FirstName = "John",
+                    LastName = "Smith",
+                    Make = "Cool New Phone",
+                    Value = 500
+                }
+            };
 
+            decimal tax = 0;
+            string insurer = "";
+            string error = "";
+            IPriceEngine priceEngine = new PriceEngine();
+            var expectedPrice = priceEngine.GetPrice(request, out tax, out insurer, out error);
+
+            Assert.AreEqual(actualPrice, expectedPrice);
         }
     }
 }
